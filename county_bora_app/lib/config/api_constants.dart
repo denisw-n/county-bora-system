4c20ecaef87e0e4caf694cec98f1d411dc361642
denisw-n <158250010+denisw-n@users.noreply.github.com>
2026-06-26 13:24:27 +0300

    updated the ip address

--- county_bora_app/lib/config/api_constants.dart
+++ county_bora_app/lib/config/api_constants.dart
@@ -1,8 +1,8 @@
 class ApiConstants {
   // Use your computer's local IP address so the phone can find the server
   // Example: 'http://192.168.1.5:8000/api'
-  static const String baseUrl = 'http://10.206.89.159:8000/api';
+  static const String baseUrl = 'http://192.168.43.123:8000/api';
 
   // This is the root address used to load images properly
-  static const String storageUrl = 'http://10.206.89.159:8000';
+  static const String storageUrl = 'http://192.168.43.123:8000';
 }
